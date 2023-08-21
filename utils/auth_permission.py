ba1ae028ad34c788bc2ba36f156583d7b3abc8c9
Zhao JingHe <64878092+yxpoxy@users.noreply.github.com>
2023-08-21 14:43:10 +0800

    Update auth_permission.py

--- utils/auth_permission.py
+++ utils/auth_permission.py
@@ -1,19 +1,24 @@
 from fastapi import Depends, Security, HTTPException
 from fastapi import Request
 
-from utils.auth_login import auth_login
+from model.permissions import User, UserRole, RolePrivilege, Role
+from model.db import dbSession
+from service.permissions import roleModel
 
 
 def auth_permission(request: Request):
-    user = auth_login(request)
 
-    permission_key = request.url.path.replace('/', '_')
+    db = roleModel()
+    user_id = int(request.headers.get("user_id"))
+    # user = auth_login(request)
+    permission_key = request.url.path
     permission = None
-    """
+    role_list = db.search_role_by_user(user_id)
+    privilege_set = db.search_privilege_by_role(role_list)
+    permission = db.check_permission(permission_key, privilege_set)
     if not permission:
         raise HTTPException(
             status_code=403,
             detail="No permission",
         )
-    """
-    return user
+    return user_id
