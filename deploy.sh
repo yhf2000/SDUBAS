7296a88afe1822c3ce1d4f30d55e600c906b9637
纆冢大帅比 <120900893+mozhong123@users.noreply.github.com>
2023-09-28 15:44:17 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 sudo su
 sudo apt update
+sudo apt install rpm
+sudo apt install dnf
 
-current_directory=$(pwd)
 sudo apt-get install -y redis-server
 sudo vim /etc/redis/redis/redis.conf
 
 sudo apt install mysql-server
 sudo /etc/init.d/mysql start
 sudo mysql_secure_installation
+
+wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio-20230920224955.0.0.x86_64.rpm -O minio.rpm
+sudo dnf install minio.rpm
+mkdir ~/minio
+chmod +x minio
+chmod 777 ~/minio
+mkdir data
+cd data
+touch minio.log
+sudo apt install mc
+nohup minio server  ~/minio/data > ~/minio/data/minio.log 2>&1 &
+
+#配置用户名和密码(将xxx替换掉)
+export MINIO_ACCESS_KEY=   xxx
+export MINIO_SECRET_KEY=   xxx
 
 pip install -r requirements.txt
 
