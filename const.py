96def40dfee756772a4a738bf9a088335c35a7f3
纆冢大帅比 <120900893+mozhong123@users.noreply.github.com>
2023-11-09 11:40:41 +0800

    变更

--- const.py
+++ const.py
@@ -1,4 +1,5 @@
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://sdubas:sdubas@localhost:3306/sdubas"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://sdubas:sdubas@localhost:3306/sdubas"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:13278403012zhao@localhost:3306/test"
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://handsome_boy:SDUBASmysql123!@43.138.34.119:3306/SDUBAS"
 development_ip = '127.0.0.1'
 server_ip = "43.138.34.119"
